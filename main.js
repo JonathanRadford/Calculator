2ef072ce1d2b8c84aa44cb3f716645e27882702d
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-11 14:51:27 +0000

    Numbers and Operators Presenting

--- main.js
+++ main.js
@@ -1,29 +1,25 @@
 let h1 = document.querySelector("h1");
+//Array.from turns the information from the HTML page into an array 
 const numberedButtons = Array.from(document.querySelectorAll(".btn-primary"));
 const operatorButtons = Array.from(document.querySelectorAll(".operator"));
-const allButtons = Array.from(document.querySelectorAll(".buttons"));
+const allButtons = Array.from(document.querySelectorAll(".button"));
 const equalButton = document.querySelector("#Calculator__Equals");
 const clearButton = document.querySelector("#Calculator__Clear");
 
+
+
 numberedButtons.map(button  => { 
     button.addEventListener("click", (buttons) => {
-        h1.innerText = buttons.target.innerText;
-        console.log(buttons.target.innerText);
-      
+        h1.innerText += buttons.target.innerText;  
 })
 });
 
 operatorButtons.map(button  =>{ 
   button.addEventListener("click", (buttons) => {
-    console.log(buttons.target.innerText);
-    h1.innerText = buttons.target.innerText;
+    h1.innerText += buttons.target.innerText;
 })
 });
 
 clearButton.addEventListener('click', function() {
     h1.innerHTML = '';
-});
-
-equalButton.addEventListener('click', function() {
-  h1.innerHTML = '';
 });
