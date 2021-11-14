bcf590cacc7b3995307070b84b0cc1c75ce74a75
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-14 11:33:07 +0000

    Added % function, and edited Calculator size

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ This project is to create a Calculator from scratch, with the end goal being a w
 
 I had issues at first whereby I could console.log the information to the screen after creating an array, but it wouldn't log to the h1. After targeting the h1 by utilising innerHTML, the next problem I faced was creating a function that would respond to the operator buttons onClick. After finding the information I needed to be able to search for the html elements on the page, I first used a Switch statement but switched to an If Else statement to figure out which operator button had been pressed, and therefore what to do with the numbers when equal was clicked. 
 
-At this point the only thing I can do is use the operators with two numbers, so my next task will be to allow more numbers to be pressed. 
+As things stand I can only use two numbers combined with an operator, but I intend to expand upon this as well as add in further useable buttons.
--- main.js
+++ main.js
@@ -1,41 +1,42 @@
-//all the selectors turned into elements
-let h1 = document.querySelector("h1");
+//all the selectors turned into variables
+const currentSum = document.querySelector("#current-input")
 //Array.from turns the information from the HTML page into an array 
 const numberedButtons = Array.from(document.querySelectorAll(".btn-primary"));
 const operatorButtons = Array.from(document.querySelectorAll(".operator"));
-const allButtons = Array.from(document.querySelectorAll(".button"));
 const equalButton = document.querySelector("#Calculator__Equals");
 const clearButton = document.querySelector("#Calculator__Clear");
-
+const percentage = document.querySelector("Calculator__Percentage");
 
 //on click the numbered Buttons find the button from the array and perform the function getClickedOperator
-numberedButtons.map(button  => { 
-  button.addEventListener("click", (button) => {
+numberedButtons.map(number  => { 
+  number.addEventListener("click", (button) => {
      findNumber(button)
 })})
 
 //on click the Operator Buttons find the button from the array and perform the function getClickedOperator
-operatorButtons.map(button  => { 
-  button.addEventListener("click", (button) => {
+operatorButtons.map(operator  => { 
+  operator.addEventListener("click", (button) => {
     findOperator(button)
 })})
-
-//On click the clear button returns an empty string to H1
-clearButton.addEventListener('click', function() {
-  h1.innerHTML = ' ';
-});
 
 //this stores the clicked number and concatenates it to another if pressed
 findNumber = (button) => {
   const number = button.target.innerHTML;
-   h1.innerHTML += number;
+  currentSum.innerHTML += number;
 }
+
+//On click the clear button returns an empty string to H1
+clearButton.addEventListener('click', function() {
+  currentSum.innerHTML = ' ';
+});
+
 
 //this takes the two strings either side of the operator
 findOperator = (button) =>{
     operator = button.target.innerHTML;
-    h1.innerHTML += " " + operator + " ";
+    currentSum.innerHTML += " " + operator + " ";
 };
+
 
 //on click equals returns the getAnswer function
 equalButton.addEventListener("click", () => findAnswer())
@@ -43,9 +44,9 @@ equalButton.addEventListener("click", () => findAnswer())
 //function that responds to the Equal button being clicked after using an operator
 findAnswer = () => {
   //created a variable answer to send information to
-  let answer;
+  let answer = " ";
   //created three seperate variables and stored which buttons and which operator have been used after .split turned them into an array
-  const numbers = h1.innerHTML.split(" " + operator + " ")
+  const numbers = currentSum.innerHTML.split(" " + operator + " ")
   //stored the two number strings from the array
   const number1 = numbers[0]
   const number2 = numbers[1]
@@ -55,6 +56,8 @@ findAnswer = () => {
     //if the operator button clicked is a string equal to "/" it divides, if not then it carries out the other statement if that one is true
     if(operator === "/"){
       answer = Number(number1) / Number(number2);
+    } else if (operator === "%"){
+      answer = (Number(number1) / 100 ) * Number(number2);
     } else if (operator === "*"){
       answer = Number(number1) * Number(number2);
     } else if (operator === "-"){
@@ -64,6 +67,6 @@ findAnswer = () => {
   } else {
      return
    }
-   return h1.innerHTML = answer;
+   return currentSum.innerHTML = answer;
 } 
 }
