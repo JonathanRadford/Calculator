0c61095350764f14a499b8b17874f04c374f1a1a
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-15 07:26:16 +0000

    MAIN.JS cleaning up

--- main.js
+++ main.js
@@ -7,16 +7,10 @@ const equalButton = document.querySelector("#Calculator__Equals");
 const clearButton = document.querySelector("#Calculator__Clear");
 const percentage = document.querySelector("Calculator__Percentage");
 
-//on click the numbered Buttons find the button from the array and perform the function getClickedOperator
+//on click the numbered Buttons find the button from the array
 numberedButtons.map(number  => { 
   number.addEventListener("click", (button) => {
      findNumber(button)
-})})
-
-//on click the Operator Buttons find the button from the array and perform the function getClickedOperator
-operatorButtons.map(operator  => { 
-  operator.addEventListener("click", (button) => {
-    findOperator(button)
 })})
 
 //this stores the clicked number and concatenates it to another if pressed
@@ -25,25 +19,28 @@ findNumber = (button) => {
   currentSum.innerHTML += number;
 }
 
-//On click the clear button returns an empty string to H1
+//on click the Operator Buttons find the button from the array
+operatorButtons.map(operator  => { 
+  operator.addEventListener("click", (button) => {
+    findOperator(button)
+})})
+
+//this takes the two strings either side of the operator
+findOperator = (button) =>{
+  operator = button.target.innerHTML;
+  currentSum.innerHTML += " " + operator + " ";
+};
+
+//On click the clear button returns an empty string to currentSum
 clearButton.addEventListener('click', function() {
   currentSum.innerHTML = ' ';
 });
 
-
-//this takes the two strings either side of the operator
-findOperator = (button) =>{
-    operator = button.target.innerHTML;
-    currentSum.innerHTML += " " + operator + " ";
-};
-
-
-//on click equals returns the getAnswer function
+//on click equals performs the getAnswer function
 equalButton.addEventListener("click", () => findAnswer())
 
 //function that responds to the Equal button being clicked after using an operator
 findAnswer = () => {
-  //created a variable answer to send information to
   let answer = " ";
   //created three seperate variables and stored which buttons and which operator have been used after .split turned them into an array
   const numbers = currentSum.innerHTML.split(" " + operator + " ")
@@ -53,7 +50,7 @@ findAnswer = () => {
 
   //if the numbers at [0] and [1] are not equal to an empty string then the if else statement comes into play
   if (numbers[0] != " " && numbers[1] != " "){
-    //if the operator button clicked is a string equal to "/" it divides, if not then it carries out the other statement if that one is true
+    //if the operator button clicked is a string equal to "/" it divides, if not then it carries out the a statement if it fits the criteria
     if(operator === "/"){
       answer = Number(number1) / Number(number2);
     } else if (operator === "%"){
